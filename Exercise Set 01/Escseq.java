c8e47de2d1474b0ad2faa3758f16313a8621c405
gil <guillermoocinagil@gmail.com>
2024-08-21 09:04:53 +0800

    Initial commit

---
+++ Exercise Set 01/Escseq.java
@@ -0,0 +1,14 @@
+public class Escseq {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            int multiple = 7 * i;
+
+            if (i % 2 == 0) {
+                System.out.println("\t" + multiple);
+            } else {
+                System.out.println(multiple);
+            }
+        }
+        
+    }
+}
---
+++ Exercise Set 01/Tempconvert.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tempconvert {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Celsius value: ");
+        Float celsius = scanner.nextFloat();
+        Float farenheit = celsius * 9 / 5 + 32;
+
+        System.out.println(celsius + " Celsius is " + farenheit + " Farenheit");
+        scanner.close();
+        
+    }
+}
