7c9290ee1439946e635ab2268b37d86ceafd33b8
gil <guillermoocinagil@gmail.com>
2024-10-07 09:10:46 +0800

    10/07/24 Exercise 2

---
+++ Exercise Sets/Exercise_Set_02/src/Car.java
@@ -0,0 +1,74 @@
+
+public class Car {
+	
+	// private properties
+	private String color;
+	private double price;
+	private char size;
+	
+	// constructor
+	public Car(String color, double price, char size) {
+		this.color = color;
+		this.price = price;
+		this.size = Character.toUpperCase(size);
+
+	}
+	
+	// getters
+	public String getColor() {
+		return color;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public char getSize() {
+		return size;
+	}
+	
+	// setters
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setSize(char size) {
+		this.size = Character.toUpperCase(size);
+	}
+	
+	// toString()
+	public String toString() {
+		String sizeDecriptor;
+		
+		switch (size) {
+		case 'S':
+			sizeDecriptor = "small";
+			break;
+		case 'M':
+			sizeDecriptor = "medium";
+			break;
+		case 'L':
+			sizeDecriptor = "large";
+			break;
+		default:
+			sizeDecriptor = "unknown";
+			break;
+		}
+		
+		return String.format("Car (%s) - P%.2f - %s", color, price, sizeDecriptor);
+	}
+	
+	public static void main(String[] args) {
+		Car car1 = new Car("red", 19999.85, 'm');
+		Car car2 = new Car("blue", 50000.00, 'L');
+		
+		System.out.println(car1);
+		System.out.println(car2);
+
+	}
+
+}
